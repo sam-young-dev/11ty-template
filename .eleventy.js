3d06e95239dc743bb28f3bd11dd117eec5154721
syoung6 <sam.young@mailbox.org>
2021-05-12 16:09:46 -0500

    add scss files and files to compile it to css

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,29 @@ const dateFilter = require('./src/filters/date-filter.js');
 const markdownFilter = require('./src/filters/markdown-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
 
+// Import transforms
+const htmlMinTransform = require("./src/transforms/html-min-transform.js");
+const parseTransform = require("./src/transforms/parse-transform.js");
+const criticalCSSTransform = require("./src/transforms/critical-css-transform.js");
+
+// Import data files
+const site = require("./src/_data/site.json");
+
 module.exports = function(config) {
   // Filters
   config.addFilter('dateFilter', dateFilter);
   config.addFilter('markdownFilter', markdownFilter);
   config.addFilter('w3DateFilter', w3DateFilter);
+
+  // Transforms
+  config.addTransform("parse", parseTransform);
+  if (site.criticalCSS) {
+    config.addTransform("critical-css", criticalCSSTransform);
+  } 
+  else {
+    // Critical will also minify
+    config.addTransform("htmlmin", htmlMinTransform);
+  }
 
   // Passthrough
   config.addPassthroughCopy('src/css');
