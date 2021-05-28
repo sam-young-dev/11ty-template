7a84f1d913b4c7960df8d4af26d3b8b956418be6
syoung6 <sam.young@email.com>
2021-05-28 16:13:33 -0500

    Feat: subscribe user after submitting subscribe form

--- functions/submission-created.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-  const { payload } = JSON.parse(event.body);
-  console.log(payload);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(payload.data)
-  };
-}
